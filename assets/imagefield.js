e16f1f3b7b8738a1e721055ffb7f99dcf59cc728
floor12 <primelifecoder@gmail.com>
2016-02-28 21:10:46 +0300

    some fixes

--- assets/imagefield.js
+++ assets/imagefield.js
@@ -74,17 +74,17 @@ $(document).ready(function () {
 //        }
 //    });
 
-    $(function () {
-        $('#fileupload').fileupload({
-            dataType: 'json',
-            done: function (e, data) {
-                $.each(data.result.files, function (index, file) {
-                    $('<p/>').text(file.name).appendTo(document.body);
-                });
-            }
-        });
-    });
-
+    //$(function () {
+    //    $('#fileupload').fileupload({
+    //        dataType: 'json',
+    //        done: function (e, data) {
+    //            $.each(data.result.files, function (index, file) {
+    //                $('<p/>').text(file.name).appendTo(document.body);
+    //            });
+    //        }
+    //    });
+    //});
+    //
 
 
 
